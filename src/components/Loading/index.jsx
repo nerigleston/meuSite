720474dbb5d7be51d3a6680cc537dd48927ed551
Neri Filho <nerifilho1256@gmail.com>
2023-11-03 16:12:40 -0300

    loading

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css"; // Importe o estilo CSS para a animação
+
+function ProgressCircle() {
+  return (
+    <div className="progress-circle"></div>
+  );
+}
+
+export default ProgressCircle;
--- src/components/certificadosAPI/index.jsx
+++ src/components/certificadosAPI/index.jsx
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react";
+import ProgressCircle from "../Loading";
 
 function APICertificados() {
   const [certificados, setCertificados] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchCertificados() {
-      const response = await fetch("https://meuportifolioapi-production.up.railway.app/certificados");
-      const data = await response.json();
-      setCertificados(data);
+      try {
+        const response = await fetch("https://meuportifolioapi-production.up.railway.app/certificados");
+        const data = await response.json();
+        setCertificados(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Erro ao buscar certificados:", error);
+        setIsLoading(false);
+      }
     }
 
     fetchCertificados();
@@ -19,17 +27,23 @@ function APICertificados() {
         <a href="https://drive.google.com/drive/u/0/folders/1MKkxyxwFR228bzpK1vIx8Cj8EJt2cj"><h1>Meus Certificados</h1></a>
       </div>
       <div className="certificados">
-        {certificados.map((certificado, index) => (
-          <div key={index}>
-            <div className="background">
-              <img src={certificado.certificadoimg} alt={`Certificado ${certificado.curso}`} />
-              <p>Instituição: {certificado.instituicao}</p>
-              <p>Competências: {certificado.materia}</p>
-              <p>Data: {certificado.data}</p>
-              <p>Curso: {certificado.curso}</p>
-            </div>
+        {isLoading ? (
+          <div className="loading">
+            <ProgressCircle /> {/* Use o componente ProgressCircle para o indicador de carregamento */}
           </div>
-        ))}
+        ) : (
+          certificados.map((certificado, index) => (
+            <div key={index}>
+              <div className="background">
+                <img src={certificado.certificadoimg} alt={`Certificado ${certificado.curso}`} />
+                <p>Instituição: {certificado.instituicao}</p>
+                <p>Competências: {certificado.materia}</p>
+                <p>Data: {certificado.data}</p>
+                <p>Curso: {certificado.curso}</p>
+              </div>
+            </div>
+          ))
+        )}
       </div>
     </>
   );
