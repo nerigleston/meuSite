aa2e95182a2db3d0422e989e04c611fbe4c724c7
Neri Filho <nerifilho1256@gmail.com>
2023-11-03 14:12:01 -0300

    dist

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './style.css'
 import Rotas from './routers/routers.jsx'
+import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../page/home';
 import Certificados from '../page/certificados';
 import AtividadeExtracurricular from '../page/atividadesExtracurricular';
-// import InserirCertificados from '../page/adicionarCertificados';
 import MeusProjetos from '../page/meusProjetos/github';
-// import Estatisticas from '../page/estatisticas';
 
-const Rotas = () => (
-    <BrowserRouter>
-        <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/certificados' element={<Certificados />} />
-            <Route exact path='/extracurricular' element={<AtividadeExtracurricular />} />
-            <Route exact path='/projetos' element={<MeusProjetos />} />
-            {/* <Route path='/estatisticas' element={<Estatisticas />} /> */}
-        </Routes>
-    </BrowserRouter>
-);
+function Rotas() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/certificados" element={<Certificados />} />
+                <Route path="/extracurricular" element={<AtividadeExtracurricular />} />
+                <Route path="/projetos" element={<MeusProjetos />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
 
-export default Rotas;
+export default Rotas
