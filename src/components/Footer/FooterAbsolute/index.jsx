5516f13180521c06468f7149ed99f1890d9ac53a
Neri Filho <nerifilho1256@gmail.com>
2023-11-04 15:48:56 -0300

    add email

--- src/components/Footer/FooterAbsolute/index.jsx
+++ src/components/Footer/FooterAbsolute/index.jsx
@@ -3,6 +3,8 @@ import './style.css'
 import Linkedin from '/src/assets/icons8-linkedin.svg'
 import GitHub from '/src/assets/icons8-github.svg'
 import Instagram from '/src/assets/icons8-instagram.svg'
+import Gmail from '/src/assets/icons8-gmail.svg'
+
 
 function FooterAbsolute() {
   return (
@@ -11,6 +13,7 @@ function FooterAbsolute() {
         <p><a className="under" href="https://www.linkedin.com/in/nerigleston/"><img src={Linkedin} alt="LinkedIn" /></a></p>
         <p><a className="under" href="https://github.com/nerigleston"><img src={GitHub} alt="GitHub" /></a></p>
         <p><a className="under" href="https://www.instagram.com/nerigleston/"><img src={Instagram} alt="GitHub" /></a></p>
+        <p><a className="under" href="mailto:nerifilho1256@gmail.com"><img src={Gmail} alt="Gmail" /></a></p>
       </div>
     </footer>
   )
--- src/components/Footer/FooterStatic/index.jsx
+++ src/components/Footer/FooterStatic/index.jsx
@@ -3,6 +3,7 @@ import './style.css'
 import Linkedin from '/src/assets/icons8-linkedin.svg'
 import GitHub from '/src/assets/icons8-github.svg'
 import Instagram from '/src/assets/icons8-instagram.svg'
+import Gmail from '/src/assets/icons8-gmail.svg'
 function FooterStatic() {
   return (
     <div>
@@ -10,6 +11,7 @@ function FooterStatic() {
         <p><a className="under" href="https://www.linkedin.com/in/nerigleston/"><img src={Linkedin} alt="LinkedIn" /></a></p>
         <p><a className="under" href="https://github.com/nerigleston"><img src={GitHub} alt="GitHub" /></a></p>
         <p><a className="under" href="https://www.instagram.com/nerigleston/"><img src={Instagram} alt="GitHub" /></a></p>
+        <p><a className="under" href="mailto:nerifilho1256@gmail.com"><img src={Gmail} alt="Gmail" /></a></p>
       </div>
     </div>
   )
